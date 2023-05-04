f49619ca9e42829808302fdf2d82443fab47e33b
Rosan7 <85017693+Rosan7@users.noreply.github.com>
2023-05-04 09:54:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -46,49 +46,6 @@ class UpdateDb(Resource):
         db.user_list.delete_one(doc)
         return jsonify(msg="User deleted successfully")
         
-# def get_all_users():
-#     '''This function returns all the user infomation which are stored in the database in form of documents
-#     The function returns a list which contains list of json objects which has user information'''
-    
-#     doc_list = list(db.user_list.find())
-#     return json.loads(json_util.dumps(doc_list))
-# @app.route("/get_users_with/<int:id>", methods=["GET"])
-# def get_users_with(id):
-#     ''' This function returns the info of a specific user with the given id in form of a json object'''
-
-#     doc_list = list(db.user_list.find({"id": id}))
-#     req_user = doc_list[0]
-#     return json.loads(json_util.dumps(req_user))
-# @app.route("/add_user",methods=["POST"])
-# def add_user():
-#     '''This adds the user information in form of document to the collection'''
-
-#     user_details = request.get_json()
-#     data = dict()
-#     data["id"] = user_details["id"]
-#     data["name"] = user_details["name"]
-#     data["email"] = user_details["email"]
-#     data["password"] = user_details["password"]
-#     db.user_list.insert_one(data)
-#     return jsonify(msg="User Added Successfully")
-# @app.route("/update_user_with_id/<int:id>",methods=["PUT"])
-# def update_user_with_id(id):
-#     '''This updates the information of the user with the specified id'''
-
-#     data = request.get_json()
-#     for key,value in data.items():
-#         value = {"$set": {key: value}}
-#         q = {"id":id}
-#         db.user_list.update_one(q,value)
-#     return jsonify(msg="User Updated Successfully")
-# @app.route("/delete_user/<int:id>",methods=["DELETE"])
-# def delete_user(id):
-#     '''This method deletes the user with the specific id'''
-
-#     doc_list = list(db.user_list.find({"id": id}))
-#     doc = doc_list[0]
-#     db.user_list.delete_one(doc)
-#     return jsonify(msg="User deleted successfully")
 api.add_resource(GetUsers,'/get_all_users')
 api.add_resource(GetUsersWithId,'/get_users_with_id/<int:id>')
 api.add_resource(AddUser,"/add_user")
